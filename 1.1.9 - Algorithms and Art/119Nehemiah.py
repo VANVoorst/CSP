9c01dd03ae097c585c2f0739175934b030d5e60b
VANVoorst <130383254+VANVoorst@users.noreply.github.com>
2025-09-30 09:04:28 -0500

    Update 119Nehemiah.py

--- 1.1.9 - Algorithms and Art/119Nehemiah.py
+++ 1.1.9 - Algorithms and Art/119Nehemiah.py
@@ -1,26 +1,29 @@
 import turtle as trtl
+#function to scale coords
+def scale_shape(name, coords, scale):
+    scaled = tuple((x * scale, y * scale) for x, y in coords)
+    trtl.addshape(name, scaled)
 
 #import/make custom turtles 
 trtl.addshape("six", ((0,8), (-2,7.7), (-3,6.8), (-4,5), (-3.8,3), (-2,2.5), (-1,2.7), (0,3), (0,4), (-1.5,5), (-1,6), (0,7), (-2,4), (-1,4), (-1,3), (-1,3))) #6
 trtl.addshape("seven", ((0,7), (-4,7), (3.8,5), (-2,3), (-1,2), (0,2), (0,3), (-1,5), (0,5))) #7
 trtl.addshape("I", ((3,-2), (0,-2), (-3,-2), (0,-2), (0,7), (3,7), (0,7), (-3,7))) #i
-trtl.addshape("N", (()))#n
-trtl.addshape("D", (()))#d
-trtl.addshape("Y", (())) #y
+#coords
+six_coords = ((0,8), (-2,7.7), (-3,6.8), (-4,5), (-3.8,3), (-2,2.5), (-1,2.7), (0,3), (0,4), (-1.5,5), (-1,6), (0,7), (-2,4), (-1,4), (-1,3), (-1,3))
+seven_coords = ((0,7), (-4,7), (3.8,5), (-2,3), (-1,2), (0,2), (0,3), (-1,5), (0,5))
+I_coords = ((3,-2), (0,-2), (-3,-2), (0,-2), (0,7), (3,7), (0,7), (-3,7))
+#scale factors
+scale_factor = 2
+scale_shape("six", six_coords, scale_factor)
+scale_shape("seven", seven_coords, scale_factor)
+scale_shape("I", I_coords, scale_factor)
 
-#trtl list 
-my_turtles = []
 
-#increase sahpe size
-'''six = trtl.Turtle(shape="six")
-six.turtlesize(2)
-seven = trtl.Turtle(shape="seven")
-seven.turtlesize(2)
-I = trtl.Turtle(shape="I")
-I.turtlesize(2)'''
+
 #color list
-turtle_shapes = ["six", "seven", "I", "N", "D", "Y"]
+turtle_shapes = ["six", "seven", "I", "six", "seven", "I"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+my_turtles = []
 
 #trtl colors and shape 
 for s in turtle_shapes:
@@ -38,10 +41,8 @@ for t in my_turtles:
   t.pendown()
   t.right(180)     
   t.forward(50)
-
-#starting postion 2
-startx = startx + 50
-starty = starty + 50
+  startx = startx + 50
+  starty = starty + 50
 
 wn = trtl.Screen()
 wn.mainloop()
