c431e137e3690f59b9df909943044caadc17f8b3
VANVoorst <130383254+VANVoorst@users.noreply.github.com>
2025-10-02 09:39:05 -0500

    Update 119Nehemiah.py
    added text box and fixed it so it no longer accepts random words for colors

--- 1.1.9 - Algorithms and Art/119Nehemiah.py
+++ 1.1.9 - Algorithms and Art/119Nehemiah.py
@@ -3,7 +3,8 @@ import turtle as trtl
 def scale_shape(name, coords, scale):
     scaled = tuple((x * scale, y * scale) for x, y in coords)
     trtl.addshape(name, scaled)
-
+test = trtl.Turtle()
+test.hideturtle()
 #import/make custom turtles 
 trtl.addshape("six", ((0,8), (-2,7.7), (-3,6.8), (-4,5), (-3.8,3), (-2,2.5), (-1,2.7), (0,3), (0,4), (-1.5,5), (-1,6), (0,7), (-2,4), (-1,4), (-1,3), (-1,3))) #6
 trtl.addshape("seven", ((0,7), (-4,7), (3.8,5), (-2,3), (-1,2), (0,2), (0,3), (-1,5), (0,5))) #7
@@ -17,7 +18,7 @@ I_coords = ((3,-2), (0,-2), (-3,-2), (0,-2), (0,7), (3,7), (0,7), (-3,7))
 person_coords = ((-3,-1), (0,2), (3,-1), (0,5), (-3,5), (3,5), (0,7), (1,7), (2,9), (0,11), (-2,9), (-1,7))
 
 #scale factors
-scale_factor = float(input("Enter a scale factor for the shapes example 2 or 5: "))
+scale_factor = float(trtl.textinput("Size","Enter a scale factor for the shapes example 2 or 5: "))
 scale_shape("six", six_coords, scale_factor)
 scale_shape("seven", seven_coords, scale_factor)
 scale_shape("I", I_coords, scale_factor)
@@ -25,11 +26,17 @@ scale_shape("person", person_coords, scale_factor)
 # ask user for colors
 turtle_colors = []
 while True:
-    color_input = input("Enter a color 6 is the max number (or type 'done' to finish): ")
+    color_input = trtl.textinput("Color", "Enter a color (6 is the max number of colors allowed type 'done' to finish): ")
     if color_input.lower() == 'done':
         break
     else:
-        turtle_colors.append(color_input)
+       
+        try:
+            test.color(color_input)
+            turtle_colors.append(color_input)
+        except trtl.TurtleGraphicsError:
+          continue
+        
 
 print("Your final list of colors:", turtle_colors)
 
