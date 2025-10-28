32245849dd01da834f9ffbab1165664ae44df7d5
VANVoorst <130383254+VANVoorst@users.noreply.github.com>
2025-10-28 09:18:07 -0500

    milestone 4
    added leaderboard functions not fully complete

--- 1.2.5 - Shall We Play a Game/1.25Nehemiah2.py
+++ 1.2.5 - Shall We Play a Game/1.25Nehemiah2.py
@@ -1,15 +1,36 @@
 import turtle as trtl
 import random as rand
+import leaderboard as lb
+
+
 
 #-----setup-----
 bird_image = "bird3.gif"
 timer = 12
 counting_down = True
+max_chars= 7
+font_setup = ("Arial", 20, "normal")
+
 #screen setup
 wn = trtl.Screen()
 wn.setup(width=0.68, height=0.75)
 wn.addshape(bird_image)
 wn.bgpic("background.gif")
+
+#leader board
+leaderboard_file_name = "a125_leaderboard.txt"
+leader_names_list = []
+leader_scores_list = []
+player_name = trtl.textinput("Name", "enter your name (max" + str(max_chars)+ " chars:")
+player_name = player_name[:max_chars]
+while "," in player_name:
+    player_name = trtl.textinput("Name", "your name can not contanin a comma (,)")
+
+leader_names_list = lb.get_names("a122_leaderboard.txt") 
+leader_scores_list = lb.get_scores("a122_leaderboard.txt")
+
+
+
 
 bird = trtl.Turtle()
 bird.penup()
@@ -22,6 +43,13 @@ timer_writer = trtl.Turtle()
 timer_writer.hideturtle()
 timer_writer.penup()
 timer_writer.goto(360, 280)
+
+# score writer
+score_writer = trtl.Turtle()
+score_writer.hideturtle()
+score_writer.penup()
+score_writer.goto(220, 170)
+score_writer.write(score, font=font_setup)
 
 #-----functions-----
 #makes the bird move
@@ -51,10 +79,10 @@ def countdown():
     timer_writer.clear()
 
     if timer <= 0:
-        timer_writer.write("Game Over!", font=("Arial", 20, "bold"))
+        timer_writer.write("Game Over!", font= font_setup)
         counting_down = False
     else:
-        timer_writer.write("Time: " + str(timer), font=("Arial", 18, "bold"))
+        timer_writer.write("Time: " + str(timer), font= font_setup)
         timer -= 1
         wn.ontimer(countdown, 1000)  
 
